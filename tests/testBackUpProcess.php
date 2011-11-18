dd3f31b41d520c3db4989d76c46e1a1a07689230
Tom Willmot <tom@humanmade.co.uk>
2011-11-18 19:20:31 +0000

    Assert the number of files in the backup archives and add tests for database only (both mysqldump and fallback) with archive fallback.

--- tests/testBackUpProcess.php
+++ tests/testBackUpProcess.php
@@ -65,6 +65,8 @@ class testBackUpProcessTestCase extends WP_UnitTestCase {
 		$this->assertFileExists( $this->backup->archive_filepath() );
 
 		$this->assertArchiveContains( $this->backup->archive_filepath(), array( 'test-data.txt', $this->backup->database_dump_filename ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 2 );
+
 
 	}
 
@@ -83,6 +85,7 @@ class testBackUpProcessTestCase extends WP_UnitTestCase {
 		$this->assertFileExists( $this->backup->archive_filepath() );
 
 		$this->assertArchiveContains( $this->backup->archive_filepath(), array( 'test-data.txt', $this->backup->database_dump_filename ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 2 );
 
 	}
 
@@ -103,6 +106,7 @@ class testBackUpProcessTestCase extends WP_UnitTestCase {
 		$this->assertFileExists( $this->backup->archive_filepath() );
 
 		$this->assertArchiveContains( $this->backup->archive_filepath(), array( 'test-data.txt' ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 1 );
 
 	}
 
@@ -122,6 +126,7 @@ class testBackUpProcessTestCase extends WP_UnitTestCase {
 		$this->assertFileExists( $this->backup->archive_filepath() );
 
 		$this->assertArchiveContains( $this->backup->archive_filepath(), array( 'test-data.txt' ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 1 );
 
 	}
 
@@ -141,6 +146,7 @@ class testBackUpProcessTestCase extends WP_UnitTestCase {
 		$this->assertFileExists( $this->backup->archive_filepath() );
 
 		$this->assertArchiveContains( $this->backup->archive_filepath(), array( $this->backup->database_dump_filename ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 1 );
 
 	}
 
@@ -160,6 +166,50 @@ class testBackUpProcessTestCase extends WP_UnitTestCase {
 		$this->assertFileExists( $this->backup->archive_filepath() );
 
 		$this->assertArchiveContains( $this->backup->archive_filepath(), array( $this->backup->database_dump_filename ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 1 );
+
+	}
+
+	/**
+	 * Test a database only backup with the mysqldump command and the zip command
+	 *
+	 * @access public
+	 * @return null
+	 */
+	function testDatabaseOnlyWithMysqldumpCommandAndZipCommand() {
+
+		$this->backup->database_only = true;
+
+		$this->assertNotEmpty( $this->backup->zip_command_path );
+		$this->assertNotEmpty( $this->backup->mysqldump_command_path );
+
+		$this->backup->backup();
+
+		$this->assertFileExists( $this->backup->archive_filepath() );
+
+		$this->assertArchiveContains( $this->backup->archive_filepath(), array( $this->backup->database_dump_filename ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 1 );
+
+	}
+
+	/**
+	 * Test a database only backup with the mysqldump fallback and the zip fallback
+	 *
+	 * @access public
+	 * @return null
+	 */
+	function testDatabaseOnlyWithMysqldumpFallbackAndZipFallback() {
+
+		$this->backup->database_only = true;
+		$this->backup->mysqldump_command_path = $this->backup->zip_command_path = false;
+
+		$this->backup->backup();
+
+		$this->assertFileExists( $this->backup->archive_filepath() );
+
+		$this->assertArchiveContains( $this->backup->archive_filepath(), array( $this->backup->database_dump_filename ) );
+		$this->assertArchiveFileCount( $this->backup->archive_filepath(), 1 );
+
 
 	}
 
